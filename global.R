ede9b2f9ac4aec0db252aa0c2f5fb79e7b50c2da
Marcelo Tibau <marcelo@tibau.org>
2017-03-06 11:33:30 -0300

    Add files via upload

---
+++ global.R
@@ -0,0 +1,8 @@
+# Libraries used
+
+library('shiny')
+
+# Codes to load data
+dfTrain1 <- readRDS(file = './data/dfTrain1.rds')
+dfTrain2 <- readRDS(file = './data/dfTrain2.rds')
+dfTrain3 <- readRDS(file = './data/dfTrain3.rds')
